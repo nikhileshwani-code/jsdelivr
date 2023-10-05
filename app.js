68a48d73109a9ea69df07841ecd311185950389b
nikhileshwani-code <84385077+nikhileshwani-code@users.noreply.github.com>
2023-10-05 13:10:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-document.getElementById("head").innerHTML='Hello world! Lets add';
-function add(){
-    var a=document.getElementById("num1").value;
-    var b=document.getElementById("num2").value;
-    var c=parseInt(a)+parseInt(b);
-    document.getElementById("result").innerHTML=c;
-}
+document.getElementById("head").innerHTML='Hello world';
+const apiUrl = 'https://www.boredapi.com/api/activity';
+
+        // Get the element where we will display the result
+        const resultElement = document.getElementById('result');
+
+        // Use fetch to get data from the API
+        fetch(apiUrl)
+            .then(response => {
+                // Check if the response is OK (status 200)
+                if (!response.ok) {
+                    throw new Error('Network response was not ok' + response.statusText);
+                }
+                return response.json(); // Parse the JSON from the response
+            })
+            .then(data => {
+                // Use the parsed JSON data
+                resultElement.textContent = data.activity; // Update the text content of our result element
+            })
+            .catch(error => {
+                // Handle errors, like network issues
+                console.error('There has been a problem with your fetch operation:', error);
+                resultElement.textContent = 'Failed to fetch activity. Please try again later.';
+            });
