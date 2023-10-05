87a26794ae8c1a6870897fc42ba4bb698e053673
nikhileshwani-code <84385077+nikhileshwani-code@users.noreply.github.com>
2023-10-05 12:24:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-document.getElementById("head").innerHTML='Helloworld';
+document.getElementById("head").innerHTML='Hello world! Lets add';
+function add(){
+    var a=document.getElementById("num1").value;
+    var b=document.getElementById("num2").value;
+    var c=parseInt(a)+parseInt(b);
+    document.getElementById("result").innerHTML=c;
+}
