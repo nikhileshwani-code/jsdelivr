82cc070eceed98f0dcd43892d46a959e577f8a47
nikhileshwani-code <84385077+nikhileshwani-code@users.noreply.github.com>
2023-10-12 12:13:03 +0530

    Create app_test.js

---
+++ app_test.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+            // Modify the content as soon as the DOM is ready
+            document.getElementsByClassName('wixui-rich-text__text')[0].innerText = 'bye';
+ });
